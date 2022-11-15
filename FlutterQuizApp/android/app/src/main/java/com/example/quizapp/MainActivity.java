6badd73bd8f56629c52592c9d8ae5c51eab9e7d2
Li28ly <114960627+Li28ly@users.noreply.github.com>
2022-11-14 22:47:13 -0300

    Add files via upload

---
+++ FlutterQuizApp/android/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.quizapp;
+
+
+import io.flutter.app.FlutterActivity;
+bedding.engine.FlutterEngine;
+
+//public class MainActivity extends FlutterActivity {
+  //@Override
+  //protected void onCreate(Bundle savedInstanceState) {
+  //super.onCreate(savedInstanceState);
+  //GeneratedPluginRegistrant.registerWith(this);
+
+  //}
+  
+public class MainActivity extends FlutterActivity {
+  // You do not need to override onCreate() in order to invoke
+  // GeneratedPluginRegistrant. Flutter now does that on your behalf.
+
+  // ...retain whatever custom code you had from before (if any).
+}
+
